ca446264027161afae4e58f3c9682982c6df1a97
elhb <erik.borgstrom@scilifelab.se>
2017-11-04 16:46:21 +0100

    in case we fail to get the md5sum just return a random unique string so that the file will not be tracked as duplicate of something its not

--- filetracker/md5_sum.py
+++ filetracker/md5_sum.py
@@ -1,9 +1,13 @@
 import hashlib
+import random
 
 # define a md5sum function
 def md5(fname):
-    hash_md5 = hashlib.md5()
-    with open(fname, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            hash_md5.update(chunk)
-    return hash_md5.hexdigest()
+    try:
+        hash_md5 = hashlib.md5()
+        with open(fname, "rb") as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                hash_md5.update(chunk)
+        return hash_md5.hexdigest()
+    except IOError:
+        return ''.join([random.choice('1234567890ASDFGHJKLZZXCVBNMQWERTYUIOPqwertyuiopasdfghjklzxcvbnm') for i in xrange(20)])
