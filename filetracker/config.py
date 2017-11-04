caf06c5cf7bc090ec9d68e98633fe810030f8b8d
elhb <eriklhb@gmail.com>
2017-11-04 03:04:27 +0000

    adding another spinner

--- filetracker/config.py
+++ filetracker/config.py
@@ -1,13 +1,17 @@
 from filetracker.spinner import Spinner
 import os
+import sys
 
 USE_SPINNER = True
 SPINNER = Spinner()
 
+sys.stderr.write('INFO :: Loading precalculated md5 sums from disk.\n')
+_s = Spinner()
 KNOWNMD5S = dict()
 if os.path.isfile('knownmd5s.tsv'):
     with open('knownmd5s.tsv') as infile:
         for line in infile:
+            _s.next()
             md5, filename, modtime, source = line.rstrip().split('\t')
             if os.path.isfile(filename) and modtime == str(os.path.getmtime(filename)):
                 KNOWNMD5S[filename] = md5
@@ -17,3 +21,4 @@ if os.path.isfile('knownmd5s.tsv'):
             if filename not in printed:
                 outfile.write('{0}\t{1}\t{2}\t{3}\n'.format(md5, filename,os.path.getmtime(filename),'K'))
                 printed[filename]=True
+sys.stderr.write('\nINFO :: Loaded.\n\n')
