8ee2010c54b3f1b4bff4e1964f136f843e133892
elhb <eriklhb@gmail.com>
2017-11-08 12:28:53 +0000

    adding memory stats to output

--- filetracker/spinner.py
+++ filetracker/spinner.py
@@ -1,6 +1,7 @@
 import sys
+import psutil
 
-def Spinner(update_intervall=100):
+def Spinner(update_intervall=100, with_memory=True, mem_intervall=100000):
      counter = 0
      while True:
           if counter%update_intervall==0:
@@ -9,5 +10,6 @@ def Spinner(update_intervall=100):
               elif counter%5 == 3: marker = '/'
               elif counter%5 == 0: marker = '-'
               sys.stderr.write('Processed: {0} {1} files/dirs \r'.format(marker,counter))
+              if with_memory and counter%mem_intervall==0:   sys.stderr.write('Processed: {0} {1} files/dirs {2}% of system memory used \r'.format(marker,counter,psutil.virtual_memory().percent))
           counter += 1
           yield counter
