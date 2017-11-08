bf70160e232b27cc9ad1eb87983ba0730ce1b850
elhb <eriklhb@gmail.com>
2017-11-08 06:53:11 +0000

    md5 sum calculator know returns None on any error

--- filetracker/md5_sum.py
+++ filetracker/md5_sum.py
@@ -1,5 +1,7 @@
 import hashlib
 import random
+import sys
+import os
 
 # define a md5sum function
 def md5(fname):
@@ -9,5 +11,6 @@ def md5(fname):
             for chunk in iter(lambda: f.read(4096), b""):
                 hash_md5.update(chunk)
         return hash_md5.hexdigest()
-    except IOError:
-        return ''.join([random.choice('1234567890ASDFGHJKLZZXCVBNMQWERTYUIOPqwertyuiopasdfghjklzxcvbnm') for i in xrange(20)])
+    except:
+        sys.stderr.write('WARNING :: Cannot get md5SUm of file {}, this it will probably mess upp the duplication tracking of the affected path.\n'.format(fname))
+        return None#''.join([random.choice('1234567890ASDFGHJKLZZXCVBNMQWERTYUIOPqwertyuiopasdfghjklzxcvbnm') for i in xrange(20)])
